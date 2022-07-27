a43a0d5357e72ad27d4a15f159e0a0b2f094a6ca
Yu, Jia (Yoga) [MEDCN] <JYu36@its.jnj.com>
2022-07-27 17:33:02 +0800

    220727 matain 120 before data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { writeFileSync, readFileSync } from 'fs';
 
+const output = `${process.cwd()}/data/data.json`;
+
 @Injectable()
 export class AppService {
   saveAll(data: Array<any>) {
-    const output = `${process.cwd()}/data/data.json`;
-    writeFileSync(output, JSON.stringify(data));
+    const exist = JSON.parse(readFileSync(output).toString());
+    const date120 = new Date();
+    date120.setDate(date120.getDate() - 121);
+    const old = exist.filter((item) => {
+      const date = new Date(item['Filing Document Ready Date']);
+      // date.setMinutes(date.getMinutes() + 480);
+      return date.getTime() < date120.getTime();
+    });
+    console.log('old: ', old);
+    const newData = old.concat(data);
+    writeFileSync(output, JSON.stringify(newData));
   }
 
   getAll() {
-    let result = readFileSync(`${process.cwd()}/data/data.json`).toString();
-    result = JSON.parse(result);
+    const result = JSON.parse(readFileSync(output).toString());
     return result;
   }
 }
